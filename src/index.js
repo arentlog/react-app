463b88d82aba900514d4632bbd7dc0f0c41e2837
Logan Arent <larent@laneautomotive.com>
2018-12-12 10:24:17 -0500

    Simple commit

--- src/index.js
+++ src/index.js
@@ -1,67 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
+import Board from './components/board.js';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
-}
+class App extends React.Component {
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+  handleClick(i) {
+    console.log('Button clicked:', i);
   }
 
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
   render() {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <Board onClick={i => this.handleClick(i)} />
         </div>
       </div>
     );
   }
 }
 
-// ========================================
-
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
